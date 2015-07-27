ad562338c29a5e21dbc46061ca449ecea30e4dbc
oimou <s1190013@gmail.com>
2015-07-28 00:20:23 +0900

    Introduce react

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,44 +1,87 @@
+/** @jsx React.DOM */
+
 "use strict";
 
-var socket = io.connect();
-var button = document.querySelector("#takemotion");
-var canvas = document.querySelector("#canvas");
-var ctx = canvas.getContext("2d");
+class Canvas extends React.Component {
+    render() {
+        let className = "canvas";
 
-socket.on("connect", function () {
-    console.log("connected");
-});
-
-socket.on("liveview:frame", function (data) {
-    var img = new Image();
-    var jpgStr = arrayBufferToBase64(data.jpg);
-
-    img.src = "data:image/png;base64," + jpgStr;
-    ctx.drawImage(img, 0, 0);
-    img = null;
-});
-
-button.addEventListener("click", function () {
-    button.disabled = true;
-    socket.emit("takemotion:start");
-}, false);
-
-socket.on("takemotion:finish", function () {
-    button.disabled = false;
-});
-
-/**
- * arrayBufferToBase64
- *
- * @param {ArrayBuffer} buffer
- * @return {String}
- */
-function arrayBufferToBase64( buffer ) {
-    var binary = ''
-    var bytes = new Uint8Array( buffer )
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] )
+        return (
+            <canvas id="canvas" className={className} width="1280" height="960"></canvas>
+        );
     }
-    return window.btoa( binary );
 }
+
+class Nav extends React.Component {
+    render() {
+        let className = "btn-takemotion";
+
+        return (
+            <div id="takemotion" className={className}>TAKEMOTION</div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        this.socket = io.connect();
+    }
+
+    componentDidMount() {
+        let button = document.querySelector("#takemotion");
+        let canvas = document.querySelector("#canvas");
+        let ctx = canvas.getContext("2d");
+
+        this.socket.on("connect", () => {
+            console.log("connected");
+        });
+
+        this.socket.on("liveview:frame", (data) => {
+            let img = new Image();
+            let jpgStr = arrayBufferToBase64(data.jpg);
+
+            img.src = "data:image/png;base64," + jpgStr;
+            ctx.drawImage(img, 0, 0);
+            img = null;
+        });
+
+        button.addEventListener("click", () => {
+            button.disabled = true;
+            this.socket.emit("takemotion:start");
+        }, false);
+
+        this.socket.on("takemotion:finish", () => {
+            button.disabled = false;
+        });
+
+        /**
+         * arrayBufferToBase64
+         *
+         * @param {ArrayBuffer} buffer
+         * @return {String}
+         */
+        function arrayBufferToBase64( buffer ) {
+            let binary = ''
+            let bytes = new Uint8Array( buffer )
+            let len = bytes.byteLength;
+            for (let i = 0; i < len; i++) {
+                binary += String.fromCharCode( bytes[ i ] )
+            }
+            return window.btoa( binary );
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Canvas />
+                <Nav />
+            </div>
+        );
+    }
+}
+
+React.render(
+    <App />,
+    document.querySelector("#app")
+);
